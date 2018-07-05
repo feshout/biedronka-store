e3ac43f0161509de9bd4df5a591228c266f25519
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-07-05 13:32:10 +0200

    Implement working version of checkout() functuion

--- checkout.sql
+++ checkout.sql
@@ -1,16 +1,36 @@
-CREATE OR REPLACE FUNCTION checkout(INTEGER)
-  RETURNS DECIMAL(10, 2) AS
-DECLARE products_in_cart CURSOR (cart_id_in INTEGER) IS SELECT product_id, amount FROM cart_item WHERE cart_id_in = cart_id;
-$BODY$
+CREATE OR REPLACE FUNCTION checkout(cart_id_to_check INTEGER) RETURNS DECIMAL(10, 2) AS $$
+DECLARE
+  cart_item_row cart_item%rowtype;
+  price_row price%rowtype;
+
+  result DECIMAL(10, 2) := 0;
+  amount INTEGER;
+  temp INTEGER;
 BEGIN
+  FOR cart_item_row IN SELECT * FROM
+    cart_item WHERE cart_item.cart_id = cart_id_to_check ORDER BY cart_item.product_id
+    LOOP
 
 
+      amount := cart_item_row.amount;
+
+      FOR price_row IN SELECT * FROM price
+            ORDER BY price.product_id ASC, price.amount DESC
+        LOOP
+          IF cart_item_row.product_id = price_row.product_id THEN
+              temp := amount / price_row.amount;
+              amount := amount - (price_row.amount * temp);
+              result := result + (temp * price_row.price);
+          END IF;
+        END LOOP;
+  END LOOP;
+
+  RETURN result;
 END
-$BODY$
-LANGUAGE PGPSQL;
+$$ LANGUAGE plpgsql;
 
-SELECT product_id, amount FROM cart_item WHERE cart_id = 2;
-SELECT product_id, price, amount FROM price WHERE product_id IN (SELECT product_id FROM cart_item WHERE cart_id = 1) ORDER BY product_id ASC, amount DESC;
+SELECT checkout(2);
+
 
 
 
